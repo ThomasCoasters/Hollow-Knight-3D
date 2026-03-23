1f061436367b52738eab5a2f7c734a3eccbcf89b
Thomas Terlouw <thomasterlouw888@gmail.com>
2026-03-23 20:07:54 +0100

    HE BE WALKIN'

---
+++ hollow-knight-3d/player/model/knight.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+@export var skeleton: Skeleton3D
+@export var bone_names: Array[String] = [
+	"Bone.002", "Bone.003", "Bone.004", "Bone.005",
+	"Bone.007", "Bone.008", "Bone.009", "Bone.010",
+	"Bone.012", "Bone.013", "Bone.014", "Bone.015",
+	"Bone.017", "Bone.018", "Bone.019", "Bone.020",
+	"Bone.022", "Bone.023", "Bone.024", "Bone.025",
+	"Bone.027", "Bone.028", "Bone.029", "Bone.030",
+	"Bone.032", "Bone.033", "Bone.034", "Bone.035",
+	"Bone.037", "Bone.038", "Bone.039", "Bone.040",
+	"Bone.042", "Bone.043", "Bone.044", "Bone.045",
+	"Bone.047", "Bone.048", "Bone.049", "Bone.050",
+	"Bone.052", "Bone.053", "Bone.054", "Bone.055",
+	"Bone.057", "Bone.058", "Bone.059", "Bone.060",
+	"Bone.062", "Bone.063", "Bone.064", "Bone.065",
+	"Bone.067", "Bone.068", "Bone.069", "Bone.070",
+	"Bone.072", "Bone.073", "Bone.074", "Bone.075",
+	"Bone.077", "Bone.078", "Bone.079", "Bone.080"
+]
+
+var speed: float = 0
