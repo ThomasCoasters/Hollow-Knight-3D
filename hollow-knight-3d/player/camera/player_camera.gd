47985b1f03e6eec1ec7556525739535bdc498d35
Thomas Terlouw <thomasterlouw888@gmail.com>
2026-03-29 21:45:59 +0200

    W.I.P camera multiple versions

--- hollow-knight-3d/player/camera/player_camera.gd
+++ hollow-knight-3d/player/camera/player_camera.gd
@@ -1,11 +1,67 @@
-extends Camera3D
+@tool
+extends SpringArm3D
 
-@export var neck_node: Marker3D
+#statechart
+@onready var state_chart: StateChart = $StateChart
+#camera type states
+@onready var camera_mode_state_machine: CompoundState = $StateChart/Camera_mode
+@onready var _3rd_person_state: AtomicState = $"StateChart/Camera_mode/3rd_person"
+@onready var _1st_person_state: AtomicState = $"StateChart/Camera_mode/1st_person"
+@onready var side_view_state: AtomicState = $StateChart/Camera_mode/side_view
+@onready var free_state: AtomicState = $StateChart/Camera_mode/free
+@onready var locked_state: AtomicState = $StateChart/Camera_mode/locked
+var camera_states: Array[AtomicState]
 
-@export_enum("3rd_person", "1st_person", "side_view", "free", "locked") var camera_mode: int
-@export var distance: Vector3 = Vector3(0, 1, 3)
-@export var camera_rotation: Vector3 = Vector3(-10, 0, 0)
+
+#the current camera mode
+@export_enum("3rd_person", "1st_person", "side_view", "free", "locked") var camera_mode: String = "3rd_person":
+	set(value):
+		camera_mode = value
+		notify_property_list_changed()
+
+#settings for the camera
+@export var mouse_sensibility: float = 0.005
+@export var location: Vector3 = Vector3.ZERO
+@export var speed: float = 50
+
+
+
 
 func _ready() -> void:
-	global_position = neck_node.global_position + distance
-	global_rotation_degrees = camera_rotation
+	if Engine.is_editor_hint():
+		return
+	
+	#put all states in an array
+	camera_states = [_3rd_person_state, _1st_person_state, side_view_state, free_state, locked_state]
+	
+	#sets the state of the correct current mode to true
+	set_camera_mode_state(camera_mode)
+
+
+#region state chart
+func set_camera_mode_state(mode_name: String):
+	print(mode_name)
+	
+	state_chart.send_event(mode_name)
+	for state: AtomicState in camera_states:
+		print(state.name , " " , state.active)
+
+#endregion
+
+#region editor experience enhancer
+#make the export vars only visible to the current selected mode
+func _validate_property(property: Dictionary) -> void:
+	#goes over every exported var
+	for current_property in property:
+		match property.name:
+			"mouse_sensibility":
+				#modes 
+				if camera_mode not in ["3rd_person", "1st_person", "free"]:
+					property.usage = PROPERTY_USAGE_NO_EDITOR
+			"location":
+				if camera_mode not in ["side_view", "locked"]:
+					property.usage = PROPERTY_USAGE_NO_EDITOR
+			"speed":
+				if camera_mode not in ["free"]:
+					property.usage = PROPERTY_USAGE_NO_EDITOR
+#endregion
